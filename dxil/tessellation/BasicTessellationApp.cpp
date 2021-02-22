ce6784438d51e19e1286eb6b1b759f802b6b7ee2
Sean Baxter <lightborn@gmail.com>
2021-02-21 21:56:59 -0500

    Added resources to tessellation

--- dxil/tessellation/BasicTessellationApp.cpp
+++ dxil/tessellation/BasicTessellationApp.cpp
@@ -435,28 +435,28 @@ void BasicTessellationApp::LoadTextures()
 {
 	auto bricksTex = std::make_unique<Texture>();
 	bricksTex->Name = "bricksTex";
-	bricksTex->Filename = L"../../Textures/bricks.dds";
+	bricksTex->Filename = L"Textures/bricks.dds";
 	ThrowIfFailed(DirectX::CreateDDSTextureFromFile12(md3dDevice.Get(),
 		mCommandList.Get(), bricksTex->Filename.c_str(),
 		bricksTex->Resource, bricksTex->UploadHeap));
 
 	auto checkboardTex = std::make_unique<Texture>();
 	checkboardTex->Name = "checkboardTex";
-	checkboardTex->Filename = L"../../Textures/checkboard.dds";
+	checkboardTex->Filename = L"Textures/checkboard.dds";
 	ThrowIfFailed(DirectX::CreateDDSTextureFromFile12(md3dDevice.Get(),
 		mCommandList.Get(), checkboardTex->Filename.c_str(),
 		checkboardTex->Resource, checkboardTex->UploadHeap));
 
 	auto iceTex = std::make_unique<Texture>();
 	iceTex->Name = "iceTex";
-	iceTex->Filename = L"../../Textures/ice.dds";
+	iceTex->Filename = L"Textures/ice.dds";
 	ThrowIfFailed(DirectX::CreateDDSTextureFromFile12(md3dDevice.Get(),
 		mCommandList.Get(), iceTex->Filename.c_str(),
 		iceTex->Resource, iceTex->UploadHeap));
 
 	auto white1x1Tex = std::make_unique<Texture>();
 	white1x1Tex->Name = "white1x1Tex";
-	white1x1Tex->Filename = L"../../Textures/white1x1.dds";
+	white1x1Tex->Filename = L"Textures/white1x1.dds";
 	ThrowIfFailed(DirectX::CreateDDSTextureFromFile12(md3dDevice.Get(),
 		mCommandList.Get(), white1x1Tex->Filename.c_str(),
 		white1x1Tex->Resource, white1x1Tex->UploadHeap));
@@ -557,15 +557,10 @@ void BasicTessellationApp::BuildDescriptorHeaps()
 
 void BasicTessellationApp::BuildShadersAndInputLayout()
 {
-	shader_data["tessVS"] = load_file("Shaders\\vert.dxil");
-	shader_data["tessHS"] = load_file("Shaders\\tesc.dxil");
-	shader_data["tessDS"] = load_file("Shaders\\tese.dxil");
-	shader_data["tessPS"] = load_file("Shaders\\frag.dxil");
-
-	shader_data["tessVS2"] = load_file("Shaders\\vert2.dxil");
-	shader_data["tessHS2"] = load_file("Shaders\\tesc2.dxil");
-	shader_data["tessDS2"] = load_file("Shaders\\tese2.dxil");
-	shader_data["tessPS2"] = load_file("Shaders\\frag2.dxil");
+	 shader_data["tessVS"] = load_file("vert.dxil");
+	 shader_data["tessHS"] = load_file("tesc.dxil");
+	 shader_data["tessDS"] = load_file("tese.dxil");
+	 shader_data["tessPS"] = load_file("frag.dxil");
 
     mInputLayout =
     {
