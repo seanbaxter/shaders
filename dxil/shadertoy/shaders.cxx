4d12800229c40b1d75f9e7ddc3f8d92506f86771
Sean Baxter <lightborn@gmail.com>
2021-07-28 12:16:39 -0400

    Removed is_class_template from dxil shadertoy

--- dxil/shadertoy/shaders.cxx
+++ dxil/shadertoy/shaders.cxx
@@ -1893,7 +1893,7 @@ struct [[
 
     trace_result_t result { };
 
-    constexpr bool is_dual = @is_class_template(tracer_t, std::pair);
+    constexpr bool is_dual = tracer_t.template == std::pair;
     if constexpr(is_dual)
       result = (pixel.x < mouse.x) ?
         tracer.first.trace(scene, ro, rd, 20, 60, MaxSteps) :
